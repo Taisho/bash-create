98d35650186716780558099ef26e3e2b550419e5
Tihomir Mitkov <mitkovtihomir@gmail.com>
2019-02-22 13:40:12 +0200

    Adding new template

--- template_map.sh
+++ template_map.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 declare -A BashTemplate
-declare templateDir="$workingDir/templates/bash"
+declare -A DefaultCode
+declare templateDir="$workingDir/templates"
 
-BashTemplate[I]="$templateDir/read-file-line-by-line"
+# - Defining template path
+# - Bash
+BashTemplate[I]="$templateDir/bash/read-file-line-by-line"
+
+# - TypeScripts
+TsTemplate[I]="$templateDir/typescript/read-file-line-by-line"
+
+# Defining default code
+DefaultCode[bash]="%I;"
